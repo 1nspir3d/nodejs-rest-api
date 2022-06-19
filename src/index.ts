d3b5338038d7506fb8112bba483a722ec4659ea3
DavydVeremchuk <davyd.v@code-care.pro>
2022-06-19 20:31:28 +0300

    feat: add root and .env files

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import { createServer } from "http";
+import'dotenv/config'
+import { IResponse, TUserPayload } from "./types";
+import * as controllers from './controllers'
+
+const {PORT} = process.env
+
+
+const server = createServer((req, res) => {
+	let rawPayload = ''
+	let payload: TUserPayload
+
+	const sendResponse = (response: IResponse) => {
+		res.statusCode = response.status
+		res.end(JSON.stringify(response))
+		return
+	}
+	res.setHeader("Content-Type", "application/json")
+
+	req.on('data', (chunk) => {
+		rawPayload += chunk
+	})
+	req.on('end', () => {
+		switch(req.method) {
+			case 'GET':
+				const getRes = controllers.getController(req.url!)
+				sendResponse(getRes)
+				break;
+			case 'POST':
+				try {
+					payload = JSON.parse(rawPayload)
+				} catch (error) {
+					const response: IResponse = {
+						status: 400,
+						payload: null,
+						message: 'Invalid JSON'
+					}
+					sendResponse(response)
+				}
+				const postRes = controllers.postController(payload)
+				sendResponse(postRes)
+				break;
+			case 'PUT':
+				try {
+					payload = JSON.parse(rawPayload)
+				} catch (error) {
+					const response: IResponse = {
+						status: 400,
+						payload: null,
+						message: 'Invalid JSON'
+					}
+					sendResponse(response)
+				}
+				const putRes = controllers.putController(req.url!, payload)
+				sendResponse(putRes)
+				break;
+			case 'DELETE':
+				const deleteRes = controllers.deleteController(req.url!)
+				sendResponse(deleteRes)
+				break;
+			default:
+				sendResponse({
+					status: 404,
+					payload: null,
+					message: 'Unknown method'
+				})
+				break;
+		}
+	})
+})
+
+server.listen(PORT)
